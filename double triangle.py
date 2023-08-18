758aa2b29add08fd626159b30b2561c9fd90f69f
Shalinipal19 <shalupal804@gmail.com>
2023-08-18 22:38:59 +0530

    trianlge

---
+++ double triangle.py
@@ -0,0 +1,22 @@
+from turtle import *
+speed ('fastest')
+pencolor('red')
+
+
+
+fd(100)
+lt(120) 
+bk(8)
+fd(100)
+lt(120) 
+bk(8)
+fd(100)
+circle(50,228)
+
+lt(120) 
+bk(8)
+
+
+
+hideturtle()
+mainloop()
